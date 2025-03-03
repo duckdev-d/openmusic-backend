68225a5bc71216d5e5efc3c6a0a30883be670dfd
Dmitry <dmiry.workinbox@gmail.com>
2025-03-03 15:12:17 -0500

    add song model

---
+++ app/models/song.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import Mapped
+from sqlalchemy import ForeignKey
+
+from core.database import Base
+from models.mixins import IdMixin
+from models.mixins import CreatedAtMixin
+
+
+class Song(IdMixin, CreatedAtMixin, Base):
+    __tablename__ = 'songs'
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    duratinon_seconds: Mapped[int]
+    title: Mapped[str]
