2d1edd0f255e0691f622142bc942561a61449a36
Dmitry <dmiry.workinbox@gmail.com>
2025-03-04 08:43:35 -0500

    fix Sessoin import

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Session
+from sqlalchemy.orm import Session
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import sessionmaker
 
