d25b1dd5deb1e8481edc115253888af726628c86
Dmitry <dmiry.workinbox@gmail.com>
2025-03-04 07:52:44 -0500

    add gwt_session and engine

--- app/core/database.py
+++ app/core/database.py
@@ -1,5 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy import Session
 from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
 
 
 class Base(DeclarativeBase):
     pass
+
+
+engine = create_engine(url=settings.DB_URL, echo=True)
+
+
+session_factory = sessionmaker(engine, class_=Session, expire_on_commit=False)
+
+
+def get_session() -> Session:
+    with session_factory() as session:
+        yield session
