8d0a41b9cebefa0e885fd5ac06fa7c8d7440b5c6
Dmitry <dmiry.workinbox@gmail.com>
2025-03-06 02:39:57 -0500

    fix typo

--- app/models/user.py
+++ app/models/user.py
@@ -2,13 +2,13 @@ from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
 
-from app.models.base import Base
+from models.base import Base
 from models.mixins import IdMixin
 from models.mixins import CreatedAtMixin
 from models.song import Song
 from models.playlist import Playlist
-from models.assotiation_tables import user_song
-from models.assotiation_tables import user_playlist
+from models.assotiation_tables.user_song import UserSong
+from models.assotiation_tables.user_playlist import UserPlaylist
 
 
 class User(IdMixin, CreatedAtMixin, Base):
@@ -18,5 +18,5 @@ class User(IdMixin, CreatedAtMixin, Base):
     password_hash: Mapped[str]
     is_admin: Mapped[bool] = mapped_column(default=False)
 
-    favourite_songs = Mapped[list[Song]] = relationship(secondary=user_song)
-    favourite_playlists = Mapped[list[Playlist]] = relationship(secondary=user_playlist)
+    favourite_songs: Mapped[list[Song]] = relationship(secondary=UserSong)
+    favourite_playlists: Mapped[list[Playlist]] = relationship(secondary=UserPlaylist)
