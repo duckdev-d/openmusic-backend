d7708a32a5674efb47ee25a17b46ddb00165c1d7
Dmitry <dmiry.workinbox@gmail.com>
2025-03-07 10:54:29 -0500

    add get_by_id, add and delete methods

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -1,12 +1,33 @@
+from typing import TypeVar
+from typing import Generic
+
 from sqlalchemy.orm import Session
 
 
-class BaseRepo:
-    def __init__(self, db: Session, table: object):
-        self.db = db
-        self.table = table
+ModelType = TypeVar('ModelType')
 
-    def get_all(self):
-        query = self.db.query(self.table)
+
+class BaseRepo(Generic[ModelType]):
+    def __init__(self, db: Session, entity: type[ModelType]):
+        self.db = db
+        self.entity = entity
+
+    def get_all(self) -> list[ModelType]:
+        query = self.db.query(self.entity)
         result = query.all()
         return result
+
+    def get_by_id(self, id: int) -> ModelType | None:
+        return self.db.query(self.entity).where(self.entity.id == id).first()
+
+    def add(self, entity: ModelType) -> ModelType:
+        self.db.add(entity)
+        self.db.commit()
+        return entity
+
+    def delete(self, entity: ModelType) -> None:
+        try:
+            self.db.delete(entity)
+            self.db.commit()
+        except:
+            raise Exception('Provided value does not exist')
