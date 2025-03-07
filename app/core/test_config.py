ba3324f1f6b7fa71040fb63b378d208d158eba18
Dmitry <dmiry.workinbox@gmail.com>
2025-03-07 02:32:08 -0500

    add test config

---
+++ app/core/test_config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings
+from pydantic_settings import SettingsConfigDict
+
+
+class TestSettings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DB_URL(self):
+        return f'postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
+
+    model_config = SettingsConfigDict(env_file='.testenv')
+
+
+test_settings = TestSettings()
