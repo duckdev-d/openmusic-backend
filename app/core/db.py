e9d0d255d5633ce52df17385c015e301d00871d4
Dmitry <dmiry.workinbox@gmail.com>
2025-03-07 12:14:38 -0500

    add set_db and drop_db functions

--- app/core/db.py
+++ app/core/db.py
@@ -22,3 +22,21 @@ def get_session() -> Generator[Session, None, None]:
         yield session
     finally:
         session.close()
+
+
+def set_db():
+    from app.models.user import User
+    from app.models.song import Song
+    from app.models.playlist import Playlist
+    from app.models.assotiation_tables.playlist_song import PlaylistSong
+    from app.models.assotiation_tables.user_playlist import UserPlaylist
+    from app.models.assotiation_tables.user_song import UserSong
+    from app.models.base import Base
+
+    Base.metadata.create_all(engine)
+
+
+def drop_db():
+    from app.models.base import Base
+
+    Base.metadata.drop_all(engine)
