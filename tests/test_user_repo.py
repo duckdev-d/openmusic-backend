0a3b0af504821e42d8a2fcbb7df20e11b058eda1
Dmitry <dmiry.workinbox@gmail.com>
2025-03-07 10:54:51 -0500

    add new tests to user repo

--- tests/test_user_repo.py
+++ tests/test_user_repo.py
@@ -1,4 +1,4 @@
-from pytest import fixture
+import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -7,16 +7,16 @@ from app.core.test_config import test_settings
 
 #  unsused imports needed for initializing models
 #  and adding them to Base.metadata
-from app.models.playlist import Playlist
+from app.models.playlist import Playlist  # noqa: F401
 from app.models.user import User
-from app.models.song import Song
-from app.models.assotiation_tables.playlist_song import PlaylistSong
-from app.models.assotiation_tables.user_playlist import UserPlaylist
-from app.models.assotiation_tables.user_song import UserSong
+from app.models.song import Song  # noqa: F401
+from app.models.assotiation_tables.playlist_song import PlaylistSong  # noqa: F401
+from app.models.assotiation_tables.user_playlist import UserPlaylist  # noqa: F401
+from app.models.assotiation_tables.user_song import UserSong  # noqa: F401
 from app.models.base import Base
 
 
-@fixture(scope='function')
+@pytest.fixture(scope='function')
 def test_db():
     db_url = test_settings.DB_URL
     engine = create_engine(db_url)
@@ -28,9 +28,14 @@ def test_db():
     Base.metadata.drop_all(engine)
 
 
-@fixture(scope='function')
+@pytest.fixture(scope='function')
 def user_repo(test_db):
     return UserRepo(test_db)
+
+
+@pytest.fixture(scope='function')
+def test_user():
+    return User(username='bob', password_hash='s564dfg8d4s6g4', is_admin=False)
 
 
 def test_get_all_empty(user_repo):
@@ -42,7 +47,9 @@ def test_get_all_empty(user_repo):
 def test_get_all_with_data(test_db, user_repo):
     test_db.add_all(
         [
-            User(username='bob', password_hash='adgfdgagad84665adfgdf', is_admin=True),
+            User(
+                username='bob', password_hash='adgfdgagad84665adfgdfjfg', is_admin=True
+            ),
             User(
                 username='cris', password_hash='adgfdgagad84665adfgdf', is_admin=False
             ),
@@ -54,3 +61,45 @@ def test_get_all_with_data(test_db, user_repo):
     assert len(result) == 2
     assert result[0].is_admin
     assert result[1].username == 'cris'
+
+
+def test_get_by_id(user_repo, test_db, test_user):
+    test_db.add(test_user)
+    test_db.commit()
+    result = user_repo.get_by_id(1)
+    print(result)
+
+    assert result is not None
+    assert result.id == 1
+    assert result.username == 'bob'
+
+
+def test_get_by_id_not_existing(user_repo):
+    result = user_repo.get_by_id(15)
+
+    assert result is None
+
+
+def test_add(user_repo, test_db, test_user):
+    result = user_repo.add(test_user)
+    user = test_db.query(User).first()
+
+    assert result.id == 1
+    assert user is not None
+    assert user.id == 1
+
+
+def test_delete(user_repo, test_db, test_user):
+    test_db.add(test_user)
+    test_db.commit()
+
+    result = user_repo.delete(test_user)
+    user = test_db.query(User).first()
+
+    assert user is None
+    assert result is None
+
+
+def test_delete_not_existing(user_repo, test_user):
+    with pytest.raises(Exception):
+        user_repo.delete(test_user)
