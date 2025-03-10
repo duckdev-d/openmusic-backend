d16041629231d519f775cff78eae2f5927916c41
Dmitry <dmiry.workinbox@gmail.com>
2025-03-10 14:04:12 -0400

    add getting user by username

--- app/repositories/user.py
+++ app/repositories/user.py
@@ -5,3 +5,7 @@ from app.models.user import User
 class UserRepo(BaseRepo[User]):
     def __init__(self, db):
         super().__init__(db, User)
+
+    def get_by_username(self, username) -> User:
+        user = self.db.query(User).where(User.username == username).first()
+        return user
