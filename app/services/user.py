f4934b28631151112d6c6c5450f1995ca4d359e8
Dmitry <dmiry.workinbox@gmail.com>
2025-03-10 14:02:22 -0400

    add get_user_by_username and hashing passwords before adding to db

--- app/services/user.py
+++ app/services/user.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.repositories.user import UserRepo
 from app.schemas.user import AddUserSchema
 from app.models.user import User
+from app.core.security import get_password_hash
 
 
 class UserService:
@@ -10,5 +11,11 @@ class UserService:
         self.repo = UserRepo(db)
 
     def add_user(self, data: AddUserSchema) -> User:
-        user_obj = User(**data.model_dump())
+        password_hash = get_password_hash(data.password)
+        user_obj = User(
+            username=data.username, password_hash=password_hash, is_admin=data.is_admin
+        )
         return self.repo.create(user_obj)
+
+    def get_user_by_username(self, username: str) -> User:
+        return self.repo.get_by_username(username)
