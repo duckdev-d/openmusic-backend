2cc31e72666bb547f9a7432ccb2f1c484caca6f4
Dmitry <dmiry.workinbox@gmail.com>
2025-03-10 14:02:42 -0400

    add jwt-related config settings

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,10 @@ class Settings(BaseSettings):
     def DB_URL(self):
         return f'postgresql+psycopg2://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}'
 
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
     model_config = SettingsConfigDict(env_file='.env')
 
 
