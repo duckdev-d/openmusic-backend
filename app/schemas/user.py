aff75d33202f7f9aa7544621025c16fdb4c8e354
Dmitry <dmiry.workinbox@gmail.com>
2025-03-10 14:03:22 -0400

    change password_hash to plain password in add user shema

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -4,10 +4,11 @@ from pydantic import constr
 
 class AddUserSchema(BaseModel):
     username: constr(min_length=3, max_length=30)  # type: ignore
-    password_hash: str
+    password: str
     is_admin: bool = False
 
 
 class ShowUserSchema(BaseModel):
     username: constr(min_length=3, max_length=30)  # type: ignore
+    password_hash: str
     is_admin: bool = False
