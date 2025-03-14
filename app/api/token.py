d490124c975e0a8b924090b2398f44f9aae584be
Dmitry <dmitry.workinbox@gmail.com>
2025-03-14 03:50:44 -0600

    add logic for authorization into security.py, remove auth service

--- app/api/token.py
+++ app/api/token.py
@@ -8,8 +8,9 @@ from fastapi import status
 from sqlalchemy.orm import Session
 
 from app.schemas.token import Token
-from app.services.auth import AuthService
 from app.core.db import get_session
+from app.core.security import authenticate_user
+from app.core.security import create_access_token
 
 
 router = APIRouter(prefix='/token', tags=['jwt'])
@@ -20,13 +21,12 @@ def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
     db: Annotated[Session, Depends(get_session)],
 ):
-    auth_service = AuthService(db)
-    user = auth_service.authenticate_user(form_data.username, form_data.password)
+    user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Incorrect username or password',
             headers={'WWW-Authenticate': 'Bearer'},
         )
-    access_token = auth_service.create_access_token(data={'sub': user.username})
+    access_token = create_access_token(data={'sub': user.username})
     return Token(access_token=access_token, token_type='bearer')
--- app/core/security.py
+++ app/core/security.py
@@ -1,10 +1,31 @@
+from typing import Annotated
+from datetime import datetime
+from datetime import timezone
+from datetime import timedelta
+
+from fastapi import Depends
+from fastapi import HTTPException
+from fastapi import status
 from fastapi.security import OAuth2PasswordBearer
+import jwt
+from jwt.exceptions import InvalidTokenError
 from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+
+from app.repositories.user import UserRepo
+from app.core.config import settings
+from app.schemas.token import TokenData
+from app.core.db import get_session
+
+
+EXPIRES_DELTA = settings.ACCESS_TOKEN_EXPIRE_MINUTES
+ALGORITHM = settings.ALGORITHM
+SECRET_KEY = settings.SECRET_KEY
 
 
 pwd_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='/api/token')
 
 
 def verify_password(plain_password, hashed_password):
@@ -13,3 +34,47 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
+
+
+def authenticate_user(
+    username: str, password: str, db: Annotated[Session, Depends(get_session)]
+):
+    repo = UserRepo(db)
+    user = repo.get_by_username(username)
+    if not user:
+        return False
+    if not verify_password(password, user.password_hash):
+        return False
+    return user
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=EXPIRES_DELTA)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def get_current_user(
+    token: Annotated[str, Depends(oauth2_scheme)],
+    db: Annotated[Session, Depends(get_session)],
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    repo = UserRepo(db)
+    user = repo.get_by_username(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
