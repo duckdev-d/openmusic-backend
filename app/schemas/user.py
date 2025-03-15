95308f8de4de4f458a472d5a1a6c745458f4c2f2
Dmitry <dmitry.workinbox@gmail.com>
2025-03-15 10:52:47 -0600

    inherit add user schema from show user schema to reduce repetition

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -2,12 +2,10 @@ from pydantic import BaseModel
 from pydantic import constr
 
 
-class AddUserSchema(BaseModel):
-    username: constr(min_length=3, max_length=30)  # type: ignore
-    password: str
-    is_admin: bool = False
-
-
 class ShowUserSchema(BaseModel):
     username: constr(min_length=3, max_length=30)  # type: ignore
     is_admin: bool = False
+
+
+class AddUserSchema(ShowUserSchema):
+    password: str
