a7f29f33c852de85eb2241c0be2329a54b2b53fc
Dmitry <dmitry.workinbox@gmail.com>
2025-03-15 09:21:57 -0600

    create add_song and _get_song_duration_seconds methods

--- app/services/song.py
+++ app/services/song.py
@@ -1,9 +1,15 @@
 import os
+from uuid import uuid4
+from io import BytesIO
 
 from sqlalchemy.orm import Session
+from mutagen.mp3 import MP3
+from fastapi import UploadFile
 
 from app.core.config import settings
 from app.repositories.song import SongRepo
+from app.schemas.song import AddSongSchema
+from app.models.song import Song
 
 
 class SongService:
@@ -17,6 +23,13 @@ class SongService:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
+    @staticmethod
+    def _get_song_duration_seconds(file: bytes) -> int:
+        file = BytesIO(file)
+        song_duration_milliseconds = MP3(file).info.length
+        song_duration_seconds = song_duration_milliseconds // 1000
+        return song_duration_seconds
+
     def _save_song_file(self, file: bytes, rel_path: str) -> None:
         abs_path = self.dir_path + rel_path
         if os.path.exists(abs_path):
@@ -29,3 +42,20 @@ class SongService:
         if not os.path.exists(abs_path):
             raise FileNotFoundError
         os.remove(abs_path)
+
+    def add_song(
+        self, file: UploadFile, user_id: int, song_data: AddSongSchema
+    ) -> None:
+        file_bytes = file.file.read()
+        file_id = str(uuid4())
+        rel_path = f'/{file_id}.mp3'
+        self._save_song_file(file_bytes, rel_path)
+        song_title = song_data.title
+        song_duration_seconds = self._get_song_duration_seconds(file_bytes)
+        song_obj = Song(
+            user_id=user_id,
+            duration_seconds=song_duration_seconds,
+            title=song_title,
+            relative_file_path=rel_path,
+        )
+        self.repo.create(song_obj)
