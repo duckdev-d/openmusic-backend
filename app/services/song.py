1288f23f54fec6f42cdb0cdcc6195235a224a0e9
Dmitry <dmitry.workinbox@gmail.com>
2025-03-15 03:20:47 -0600

    add method for removig song file

--- app/services/song.py
+++ app/services/song.py
@@ -1,10 +1,14 @@
 import os
 
+from sqlalchemy.orm import Session
+
 from app.core.config import settings
+from app.repositories.song import SongRepo
 
 
 class SongService:
-    def __init__(self):
+    def __init__(self, db: Session):
+        self.repo = SongRepo(db)
         self.dir_path = settings.SONGS_DIRECTORY_PATH
         SongService._create_dir_if_not_exists(self.dir_path)
 
@@ -13,11 +17,15 @@ class SongService:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
 
-    def save_song_file(self, file: bytes, rel_path: str) -> None:
+    def _save_song_file(self, file: bytes, rel_path: str) -> None:
         abs_path = self.dir_path + rel_path
-
         if os.path.exists(abs_path):
             raise FileExistsError
-
         with open(abs_path, 'wb') as f:
             f.write(file)
+
+    def _remove_song_file(self, rel_path: str) -> None:
+        abs_path = self.dir_path + rel_path
+        if not os.path.exists(abs_path):
+            raise FileNotFoundError
+        os.remove(abs_path)
