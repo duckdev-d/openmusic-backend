1f3d7f6527bdedc5cde2d66728af23f1aacee4ae
Dmitry <dmitry.workinbox@gmail.com>
2025-03-16 01:34:47 -0600

    allow build model from attributes

--- app/schemas/user.py
+++ app/schemas/user.py
@@ -1,8 +1,11 @@
 from pydantic import BaseModel
 from pydantic import constr
+from pydantic import ConfigDict
 
 
 class ShowUserSchema(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
     username: constr(min_length=3, max_length=30)  # type: ignore
     is_admin: bool = False
 
