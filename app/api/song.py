ecbcef8b0fd64aa050760f6f3fe853088aee9d53
Dmitry <dmitry.workinbox@gmail.com>
2025-03-16 01:35:52 -0600

    add route for getting all songs

--- app/api/song.py
+++ app/api/song.py
@@ -11,6 +11,7 @@ from app.core.db import get_session
 from app.core.security import get_current_user
 from app.services.song import SongService
 from app.schemas.song import AddSongSchema
+from app.schemas.song import ShowSongSchema
 
 
 router = APIRouter(prefix='/songs', tags=['songs'])
@@ -27,3 +28,9 @@ def upload_song(
     song_data = AddSongSchema(title=title)
     song_service.add_song(file=file, user_id=current_user.id, song_data=song_data)
     return 'created'
+
+
+@router.get('/', response_model=list[ShowSongSchema])
+def get_all_songs(db: Annotated[Session, Depends(get_session)]):
+    song_service = SongService(db)
+    return song_service.get_all_songs()
