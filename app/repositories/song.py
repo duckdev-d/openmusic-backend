a7810d5919633d320274e165db14d9723fba3dec
Dmitry <dmitry.workinbox@gmail.com>
2025-03-16 01:33:02 -0600

    redefine get_all method to also load song artist

--- app/repositories/song.py
+++ app/repositories/song.py
@@ -1,3 +1,5 @@
+from sqlalchemy.orm import joinedload
+
 from app.repositories.base import BaseRepo
 from app.models.song import Song
 
@@ -5,3 +7,7 @@ from app.models.song import Song
 class SongRepo(BaseRepo[Song]):
     def __init__(self, db):
         super().__init__(db, Song)
+
+    def get_all(self):
+        query = self.db.query(Song).options(joinedload(Song.artist))
+        return query.all()
