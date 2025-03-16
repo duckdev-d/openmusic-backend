bc730567c77d88a6d7d03f20e482f62b910fe4bc
Dmitry <dmitry.workinbox@gmail.com>
2025-03-16 06:33:43 -0600

    add delete_song method

--- app/services/song.py
+++ app/services/song.py
@@ -6,12 +6,15 @@ from sqlalchemy.orm import Session
 from mutagen.mp3 import MP3
 from fastapi import UploadFile
 from fastapi.responses import FileResponse
+from fastapi import Response
+from fastapi import status
 
 from app.core.config import settings
 from app.repositories.song import SongRepo
 from app.schemas.song import AddSongSchema
 from app.schemas.song import ShowSongSchema
 from app.models.song import Song
+from app.models.user import User
 
 
 class SongService:
@@ -61,12 +64,30 @@ class SongService:
         )
         self.repo.create(song_obj)
 
-    def get_all_songs(self) -> list[Song]:
+    def get_all_songs(self) -> list[ShowSongSchema]:
         songs = self.repo.get_all()
         return [ShowSongSchema.model_validate(song) for song in songs]
 
-    def get_song_file_by_id(self, song_id) -> FileResponse:
+    def get_song_file(self, song_id: int) -> FileResponse | Response:
         song = self.repo.get_by_id(song_id)
+        if not song:
+            return Response(
+                f'Song with id {song_id} does not exist',
+                status_code=status.HTTP_404_NOT_FOUND,
+            )
         rel_path = song.relative_file_path
         abs_path = self.dir_path + rel_path
         return FileResponse(abs_path)
+
+    def delete_song(self, song_id: int, current_user: User) -> None:
+        song = self.repo.get_by_id(song_id)
+        if not song:
+            return Response(
+                f'Song with id {song_id} does not exist',
+                status_code=status.HTTP_404_NOT_FOUND,
+            )
+        if not (current_user.id == song.artist.id or current_user.is_admin):
+            raise PermissionError('You do not have the right to delete that song')
+        rel_path = song.relative_file_path
+        self.repo.delete(song)
+        self._remove_song_file(rel_path)
