ec95ace97de2b11a972e352b899621bfb74616ef
Dmitry <dmitry.workinbox@gmail.com>
2025-04-05 04:41:14 -0600

    import all models on module import to ensure threir initialization

---
+++ app/models/__init__.py
@@ -0,0 +1,6 @@
+from app.models.user import User
+from app.models.song import Song
+from app.models.playlist import Playlist
+from app.models.assotiation_tables.playlist_song import PlaylistSong
+from app.models.assotiation_tables.user_playlist import UserPlaylist
+from app.models.assotiation_tables.user_song import UserSong
